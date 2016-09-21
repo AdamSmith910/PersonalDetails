5dd8ce1fbe60a52a09552fc8fb0bff2f92d65490
Joe Hatcher <hatcher31@marshall.edu>
2016-09-21 15:12:49 -0400

    age test

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ end
 
 post '/users_name' do 
   name = params[:user_name]
-  "Hello #{name}"
+  redirect '/age?users_name=' + name # looking for its next parameter
 end
 
+get '/age' do
+  name = params[:users_name]
+  erb :get_age, :locals => {:name => name} # 
+end
+
+post '/age' do
+  age = params[:age]
+  name = params[:retrieved_name]
+  "Hello #{name}, you are #{age} years old"
+end
